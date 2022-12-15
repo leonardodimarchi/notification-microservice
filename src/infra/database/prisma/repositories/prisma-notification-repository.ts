56e80ad3b7e080963f6dc28cbb087f470ce4eb4c
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2022-12-15 19:38:08 -0300

    feat: implementing prisma-notification-repository

--- src/infra/database/prisma/repositories/prisma-notification-repository.ts
+++ src/infra/database/prisma/repositories/prisma-notification-repository.ts
@@ -7,22 +7,52 @@ import { PrismaNotificationMapper } from "../mappers/prisma-notification.mapper"
 @Injectable()
 export class PrismaNotificationRepository implements NotificationRepository {
     constructor(
-        private readonly prismaService: PrismaService,
+        private readonly prisma: PrismaService,
     ) { }
 
+    async findManyByRecipientId(recipientId: string): Promise<Notification[]> {
+        const notifications = await this.prisma.notification.findMany({
+            where: {
+                recipientId,
+            }
+        });
+
+        return notifications.map(PrismaNotificationMapper.toDomain);
+    }
+
     async findById(notificationId: string): Promise<Notification | null> {
-        throw new Error("Method not implemented.");
+        const notification = await this.prisma.notification.findUnique({
+            where: {
+                id: notificationId,
+            }
+        });
+
+        if (!notification)
+            return null
+
+        return PrismaNotificationMapper.toDomain(notification);
+    }
+
+    async countByRecipientId(recipientId: string): Promise<number> {
+        return await this.prisma.notification.count({
+            where: { recipientId },
+        });
     }
 
     async create(notification: Notification): Promise<void> {
         const raw = PrismaNotificationMapper.toPrisma(notification);
 
-        await this.prismaService.notification.create({
+        await this.prisma.notification.create({
             data: raw,
         });
     }
 
     async save(notification: Notification): Promise<void> {
-        throw new Error("Method not implemented.");
+        await this.prisma.notification.update({
+            data: PrismaNotificationMapper.toPrisma(notification),
+            where: {
+                id: notification.id,
+            }
+        });
     }
 }
