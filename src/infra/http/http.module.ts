d1397fe87ac1fbb8836b503cf7841fc1eec7cfc4
Leonardo Dimarchi <leonardodimarchi@outlook.com>
2022-12-15 19:42:43 -0300

    feat: adding missing providers at the http module

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -2,12 +2,24 @@ import { Module } from "@nestjs/common";
 import { SendNotification } from "@application/usecases/send-notification";
 import { DatabaseModule } from "../database/database.module";
 import { NotificationController } from "./controllers/notification.controller";
+import { GetRecipientNotificationCount } from "@application/usecases/get-recipient-notification-count";
+import { CancelNotification } from "@application/usecases/cancel-notification";
+import { GetRecipientNotifications } from "@application/usecases/get-recipient-notifications";
+import { ReadNotification } from "@application/usecases/read-notification";
+import { UnreadNotification } from "@application/usecases/unread-notification";
 
 @Module({
     imports: [
         DatabaseModule,
     ],
     controllers: [NotificationController],
-    providers: [SendNotification]
+    providers: [
+        SendNotification,
+        CancelNotification,
+        GetRecipientNotificationCount,
+        GetRecipientNotifications,
+        ReadNotification,
+        UnreadNotification,
+    ]
 })
 export class HttpModule { }
